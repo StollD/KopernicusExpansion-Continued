89eb0e838636d8a0eb0d1a6f55916533b052c0d8
Dorian Stoll <dorian.stoll@tmsp.io>
2017-12-08 13:36:53 +0100

    Add VertexHeightMap32

---
+++ src/KEX-VertexHeightMap32/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KEX-VertexHeightMap32")]
+[assembly: AssemblyDescription("A heightmap PQSMod that supports more than one channel")]
+[assembly: AssemblyProduct("KEX-VertexHeightMap32")]
+[assembly: AssemblyCopyright("Copyright © Thomas P. 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8CDE04F9-63DE-4805-9776-635906680E5A")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssembly("KopernicusExpansion.VertexHeightMap32", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
---
+++ src/KEX-VertexHeightMap32/VertexHeightMap32.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Kopernicus;
+using Kopernicus.Configuration;
+using Kopernicus.Configuration.ModLoader;
+
+namespace KopernicusExpansion
+{
+    namespace VertexHeightMap32
+    {
+        public class VertexHeightMap32 : ModLoader<PQSMod_VertexHeightMap32>
+        {    
+            // The map texture for the planet
+            [ParserTarget("map")]
+            public MapSOParser_GreyScale<MapSO> heightMap
+            {
+                get { return mod.heightMap; }
+                set { mod.heightMap = value; }
+            }
+
+            // Height map offset
+            [ParserTarget("offset")]
+            public NumericParser<Double> heightMapOffset 
+            {
+                get { return mod.heightMapOffset; }
+                set { mod.heightMapOffset = value; }
+            }
+
+            // Height map offset
+            [ParserTarget("deformity")]
+            public NumericParser<Double> heightMapDeformity
+            {
+                get { return mod.heightMapDeformity; }
+                set { mod.heightMapDeformity = value; }
+            }
+
+            // Height map offset
+            [ParserTarget("scaleDeformityByRadius")]
+            public NumericParser<Boolean> scaleDeformityByRadius
+            {
+                get { return mod.scaleDeformityByRadius; }
+                set { mod.scaleDeformityByRadius = value; }
+            }
+            
+            // Which channels of the map should be used?
+            [ParserTarget("depth")]
+            public EnumParser<MapSO.MapDepth> depth
+            {
+                get { return mod.depth; }
+                set { mod.depth = value; }
+            }
+        }
+    }
+}

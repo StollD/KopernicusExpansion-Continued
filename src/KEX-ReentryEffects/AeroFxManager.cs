0f919198287ee28d15b077572ea9e68eeb2e82c7
Dorian Stoll <dorian.stoll@tmsp.io>
2018-01-14 00:43:11 +0100

    Reentry Effects

---
+++ src/KEX-ReentryEffects/AeroFxManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Kopernicus;
+using UnityEngine;
+
+namespace KopernicusExpansion
+{
+    namespace ReentryEffects
+    {
+        [KSPAddon(KSPAddon.Startup.Flight, false)]
+        public class AeroFxManager : MonoBehaviour
+        {
+            /// <summary>
+            /// The KSP effect controller we are editing
+            /// </summary>
+            public AerodynamicsFX aeroFX;
+
+            /// <summary>
+            /// The default FX state for reentry.
+            /// </summary>
+            public static AeroFXState ReentryHeat;
+
+            /// <summary>
+            /// The default FX state for flying inside of the atmosphere
+            /// </summary>
+            public static AeroFXState Condensation;
+            
+            void Awake()
+            {
+                aeroFX = Resources.FindObjectsOfTypeAll<AerodynamicsFX>()[0];
+                GameEvents.onDominantBodyChange.Add(OnDominantBodyChange);
+                
+                // Check if we already stored the default values
+                if (ReentryHeat == null)
+                {
+                    ReentryHeat = aeroFX.ReentryHeat;
+                }
+                if (Condensation == null)
+                {
+                    Condensation = aeroFX.Condensation;
+                }
+            }
+
+            void OnDestroy()
+            {
+                GameEvents.onDominantBodyChange.Remove(OnDominantBodyChange);
+            }
+
+            /// <summary>
+            /// This method gets called when the vessel enters the SOI of a new body. Here we edit the FX controller, so 
+            /// it uses the effects defined for this body.
+            /// </summary>
+            void OnDominantBodyChange(GameEvents.FromToAction<CelestialBody, CelestialBody> data)
+            {
+                // The body the vessel switched to
+                CelestialBody body = data.to;
+
+                if (body.Has("reentryHeat"))
+                {
+                    aeroFX.ReentryHeat = body.Get<AeroFXState>("reentryHeat");
+                }
+                else
+                {
+                    aeroFX.ReentryHeat = ReentryHeat;
+                }
+                
+                if (body.Has("condensation"))
+                {
+                    aeroFX.Condensation = body.Get<AeroFXState>("condensation");
+                }
+                else
+                {
+                    aeroFX.Condensation = Condensation;
+                }
+            }
+        }
+    }
+}
---
+++ src/KEX-ReentryEffects/Configuration/AeroFxLoader.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Kopernicus;
+
+namespace KopernicusExpansion
+{
+    namespace ReentryEffects
+    {
+        namespace Configuration
+        {
+            /// <summary>
+            /// Loads an AeroFX from a Kopernicus Body config
+            /// </summary>
+            [RequireConfigType(ConfigType.Node)]
+            public class AeroFxLoader
+            {
+                [RequireConfigType(ConfigType.Node)]
+                public class MinMaxFloatParser
+                {
+                    // The value that is being parsed
+                    public MinMaxFloat Value;
+
+                    [ParserTarget("min")]
+                    public NumericParser<Single> min
+                    {
+                        get { return Value.min; }
+                        set { Value.min = value; }
+                    }
+
+                    public MinMaxFloatParser()
+                    {
+                        Value = new MinMaxFloat();
+                    }
+
+                    [ParserTarget("max")]
+                    public NumericParser<Single> max
+                    {
+                        get { return Value.max; }
+                        set { Value.max = value; }
+                    }
+
+                    public static implicit operator MinMaxFloat(MinMaxFloatParser parser)
+                    {
+                        return parser.Value;
+                    }
+
+                    public static implicit operator MinMaxFloatParser(MinMaxFloat value)
+                    {
+                        return new MinMaxFloatParser {Value = value};
+                    }
+                }
+                
+                [RequireConfigType(ConfigType.Node)]
+                public class MinMaxColorParser
+                {
+                    // The value that is being parsed
+                    public MinMaxColor Value;
+
+                    [ParserTarget("min")]
+                    public ColorParser min
+                    {
+                        get { return Value.min; }
+                        set { Value.min = value; }
+                    }
+
+                    [ParserTarget("max")]
+                    public ColorParser max
+                    {
+                        get { return Value.max; }
+                        set { Value.max = value; }
+                    }
+
+                    public MinMaxColorParser()
+                    {
+                        Value = new MinMaxColor();
+                    }
+
+                    public static implicit operator MinMaxColor(MinMaxColorParser parser)
+                    {
+                        return parser.Value;
+                    }
+
+                    public static implicit operator MinMaxColorParser(MinMaxColor value)
+                    {
+                        return new MinMaxColorParser {Value = value};
+                    }
+                }
+                
+                // The effect that is being parsed
+                public AeroFXState Value;
+
+                [ParserTarget("airspeedNoisePitch")]
+                public MinMaxFloatParser airspeedNoisePitch
+                {
+                    get { return Value.airspeedNoisePitch; }
+                    set { Value.airspeedNoisePitch = value; }
+                }
+
+                [ParserTarget("airspeedNoiseVolume")]
+                public MinMaxFloatParser airspeedNoiseVolume
+                {
+                    get { return Value.airspeedNoiseVolume; }
+                    set { Value.airspeedNoiseVolume = value; }
+                }
+
+                [ParserTarget("edgeFade")]
+                public MinMaxFloatParser edgeFade
+                {
+                    get { return Value.edgeFade; }
+                    set { Value.edgeFade = value; }
+                }
+
+                [ParserTarget("falloff1")]
+                public MinMaxFloatParser falloff1
+                {
+                    get { return Value.falloff1; }
+                    set { Value.falloff1 = value; }
+                }
+
+                [ParserTarget("falloff2")]
+                public MinMaxFloatParser falloff2
+                {
+                    get { return Value.falloff2; }
+                    set { Value.falloff2 = value; }
+                }
+
+                [ParserTarget("falloff3")]
+                public MinMaxFloatParser falloff3
+                {
+                    get { return Value.falloff3; }
+                    set { Value.falloff3 = value; }
+                }
+
+                [ParserTarget("intensity")]
+                public MinMaxFloatParser intensity
+                {
+                    get { return Value.intensity; }
+                    set { Value.intensity = value; }
+                }
+
+                [ParserTarget("length")]
+                public MinMaxFloatParser length
+                {
+                    get { return Value.length; }
+                    set { Value.length = value; }
+                }
+
+                [ParserTarget("lightPower")]
+                public MinMaxFloatParser lightPower
+                {
+                    get { return Value.lightPower; }
+                    set { Value.lightPower = value; }
+                }
+
+                [ParserTarget("wobble")]
+                public MinMaxFloatParser wobble
+                {
+                    get { return Value.wobble; }
+                    set { Value.wobble = value; }
+                }
+
+                [ParserTarget("color")]
+                public MinMaxColorParser color
+                {
+                    get { return Value.color; }
+                    set { Value.color = value; }
+                }
+
+                public static implicit operator AeroFXState(AeroFxLoader parser)
+                {
+                    return parser.Value;
+                }
+
+                public static implicit operator AeroFxLoader(AeroFXState value)
+                {
+                    return new AeroFxLoader {Value = value};
+                }
+            }
+        }
+
+    }
+}

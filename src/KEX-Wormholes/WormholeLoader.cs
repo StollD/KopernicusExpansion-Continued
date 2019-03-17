bc8865b5447269ae9f903879360ad7603d451ade
DeltaDizzy <EJBraswell@gmail.com>
2019-03-17 18:54:50 -0500

    add entry and exit duration parsers

--- src/KEX-Wormholes/WormholeLoader.cs
+++ src/KEX-Wormholes/WormholeLoader.cs
@@ -64,7 +64,15 @@ namespace KopernicusExpansion
             [ParserTarget("entryMsgDuration")]
             public NumericParser<UInt32> entryMsgDuration
             {
-                
+                get { return Value.entryMsgDuration; }
+                set { Value.entryMsgDuration = value; }
+            }
+            
+            [ParserTarget("exitMsgDuration")]
+            public NumericParser<UInt32> exitMsgDuration
+            {
+                get { return Value.exitMsgDuration; }
+                set { Value.exitMsgDuration = value; }
             }
             
             
